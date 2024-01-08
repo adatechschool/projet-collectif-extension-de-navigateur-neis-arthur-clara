2dece9608580a96a077991d7e169da65ac3df089
Néïs <neis.cros@gmail.com>
2024-01-08 16:50:27 +0100

    tous les onglets

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,22 @@
 document.getElementById('cadre').addEventListener('click', async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id, allFrames : true },
-      func: () => {
-        document.body.style.border = '2px solid red';
-      },
+    //let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    // chrome.scripting.executeScript({
+    //   target: { tabId: tab.id, allFrames : true },
+    //   func: () => {
+    //     document.body.style.border = '2px solid red';
+    //   },
+    // });
+
+    var tabs = await chrome.tabs.query({});
+    tabs.forEach(function (tab) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id, allFrames : true },
+            func: () => {
+              document.body.style.border = '2px solid red';
+            },
+          });
     });
+   
   });
+
+ 
