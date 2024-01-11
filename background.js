9158ba356d9add39f6476a9fa12704da2ae9c2ef
Néïs <neis.cros@gmail.com>
2024-01-11 14:55:16 +0100

    okbold

--- background.js
+++ background.js
@@ -1,8 +1,20 @@
-// chrome.action.onClicked.addListener((tab) => {
-//     chrome.scripting.executeScript({
-//       target: {tabId: tab.id},
-//       files: ['bold-script.js']
-//     });
-//   })
+// Écoutez les événements de création et de mise à jour des onglets
+chrome.tabs.onCreated.addListener(function (tab) {
+    handleToggleState(tab.id);
+});
 
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+        handleToggleState(tabId);
+    }
+});
 
+function handleToggleState(tabId) {
+    // Charger l'état sauvegardé depuis le stockage local
+    chrome.storage.local.get(['toggleState'], function (result) {
+        var savedToggleState = result.toggleState;
+        
+        // Envoyer un message au nouvel onglet avec l'état du toggle
+        chrome.tabs.sendMessage(tabId, { toggleState: savedToggleState });
+    });
+}
---
+++ contentbold.js
@@ -0,0 +1,46 @@
+//récupération du message du bold.js pour voir si le toggle est activé ou non 
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.toggleState !== undefined) {
+            // Réagir en fonction de l'état du toggle
+            if (request.toggleState) {
+                console.log('Toggle actif');
+                formatWordsInTabs();
+            } else {
+                console.log('Toggle pas actif');
+                removeWordFormatting();
+            }
+        }
+    }
+);
+
+
+
+
+
+
+//bionic reading
+function formatWordsInTabs() {
+    const allParagraphs = document.querySelectorAll("p, li, strong, em, a"); //récupération de tous les paragraphes du HTML + li
+    allParagraphs.forEach(paragraph => { //pour chaque paragraphe 
+        const textNodes = Array.from(paragraph.childNodes).filter(node => node.nodeType === Node.TEXT_NODE); //récupération de tout le text Node = texte sans lien HTTP
+        textNodes.forEach(node => { //pour tout les textes nodes 
+        const words = node.textContent.split(" "); //récupération de chaque mots
+        const formattedWords = words.map(w => `<b>${w.slice(0, Math.ceil(w.length / 2))}</b>${w.slice(Math.ceil(w.length / 2))}`); //formatage des mots pour la première moitié en gras
+        const span = document.createElement("span"); //création d'un élément span 
+        span.innerHTML = formattedWords.join(" "); //intégration du span dans le body HTML
+        paragraph.replaceChild(span, node); //remplacement du paragraphe par tout les span avec le paramètre node (sans lien http)
+        });
+    });
+}
+
+
+//remove l'état initial
+function removeWordFormatting() {
+    const allSpans = document.querySelectorAll("p > span, li > span, strong > span, em > span, a > span"); //sélection de tous les enfants de p (et autres) qui sont des span crée dans le formatWordsInTabs
+    allSpans.forEach(span => { //pour chaque span sélectionné 
+    const paragraph = span.parentNode; //récupération du parent de Node de span
+    paragraph.replaceChild(document.createTextNode(span.textContent), span); //Remplacement du span par le parent initial
+    });
+}
+
